b4ce4c14db1c3daafc44eed91fc3f9de22673459
Gagandeep Bhardwaj <gbhardwaj572@gmail.com>
2024-04-05 22:57:01 +0530

    login completed

--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { RiLock2Fill } from "react-icons/ri";
+import { Link, Navigate } from "react-router-dom";
+import { FaRegUser } from "react-icons/fa";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Context } from "../../main";
+import "./Auth.css";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("");
 
-export default Login
+  const { isAuthorized, setIsAuthorized } = useContext(Context);
+
+  const handleLogin = async (e) => {
+   
+  };
+
+  if(isAuthorized){
+    return <Navigate to={'/'}/>
+  }
+
+  return (
+    <>
+      <section className="authPage">
+        <div className="container">
+          <div className="header">
+            {/* <img src="/login_hb.png" alt="logo" /> */}
+            <h3>Login to your account</h3>
+          </div>
+          <form>
+            <div className="inputTag">
+              <label>Login As</label>
+              <div>
+                <select value={role} onChange={(e) => setRole(e.target.value)}>
+                  <option value="">Select Role</option>
+                  <option value="Employer">Employer</option>
+                  <option value="Job Seeker">Job Seeker</option>
+                </select>
+                <FaRegUser />
+              </div>
+            </div>
+            <div className="inputTag">
+              <label>Email Address</label>
+              <div>
+                <input
+                  type="email"
+                  placeholder="example@email.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <MdOutlineMailOutline />
+              </div>
+            </div>
+            <div className="inputTag">
+              <label>Password</label>
+              <div>
+                <input
+                  type="password"
+                  placeholder="Your Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <RiLock2Fill />
+              </div>
+            </div>
+            <button type="submit" onClick={handleLogin}>
+              Login
+            </button>
+            <Link to={"/register"}>Register Now</Link>
+          </form>
+        </div>
+        <div className="banner">
+          <img src="/login_hb.png" alt="login"/>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Login;
