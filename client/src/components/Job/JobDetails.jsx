ca1dee318b9a0ccdccbcc76edfbdbcdfb100f034
Akash Thappa <akashthappa21@gmail.com>
2024-04-06 04:48:03 +0530

    Job Details Done

--- client/src/components/Job/JobDetails.jsx
+++ client/src/components/Job/JobDetails.jsx
@@ -11,65 +11,68 @@ const JobDetails = () => {
 
   const { isAuthorized, user } = useContext(Context);
 
-//   useEffect(() => {
-//     axios
-//       .get(`http://localhost:3000/job/${id}`, {
-//         withCredentials: true,
-//       })
-//       .then((res) => {
-//         setJob(res.data.job);
-//       })
-//       .catch((error) => {
-//         navigateTo("/notfound");
-//       });
-//   }, []);
+  //   useEffect(() => {
+  //     axios
+  //       .get(`http://localhost:3000/job/${id}`, {
+  //         withCredentials: true,
+  //       })
+  //       .then((res) => {
+  //         setJob(res.data.job);
+  //       })
+  //       .catch((error) => {
+  //         navigateTo("/notfound");
+  //       });
+  //   }, []);
 
-//   if (!isAuthorized) {
-//     navigateTo("/login");
-//   }
+  //   if (!isAuthorized) {
+  //     navigateTo("/login");
+  //   }
 
   return (
     <section className="jobDetail page">
       <div className="container">
         <h3>Job Details</h3>
-        <div className="banner">
-          <p>
-            Title: <span> {job.title}</span>
-          </p>
-          <p>
-            Category: <span>{job.category}</span>
-          </p>
-          <p>
-            Country: <span>{job.country}</span>
-          </p>
-          <p>
-            City: <span>{job.city}</span>
-          </p>
-          <p>
-            Location: <span>{job.location}</span>
-          </p>
-          <p>
-            Description: <span>{job.description}</span>
-          </p>
-          <p>
-            Job Posted On: <span>{job.jobPostedOn}</span>
-          </p>
-          <p>
-            Salary:{" "}
-            {job.fixedSalary ? (
-              <span>{job.fixedSalary}</span>
+        <div className="jobDetailForm">
+          <div className="banner">
+            <p>
+              Title: <span> {job.title}</span>
+            </p>
+            <p>
+              Category: <span>{job.category}</span>
+            </p>
+            <p>
+              Country: <span>{job.country}</span>
+            </p>
+            <p>
+              City: <span>{job.city}</span>
+            </p>
+            <p>
+              Location: <span>{job.location}</span>
+            </p>
+            <p>
+              Description: <span>{job.description}</span>
+            </p>
+            <p>
+              Job Posted On: <span>{job.jobPostedOn}</span>
+            </p>
+            <p>
+              Salary:{" "}
+              {job.fixedSalary ? (
+                <span>{job.fixedSalary}</span>
+              ) : (
+                <span>
+                  {job.salaryFrom} - {job.salaryTo}
+                </span>
+              )}
+            </p>
+            {user && user.role === "Employer" ? (
+              <></>
             ) : (
-              <span>
-                {job.salaryFrom} - {job.salaryTo}
-              </span>
+              <Link to={`/application/${job._id}`}>Apply Now</Link>
             )}
-          </p>
-          {user && user.role === "Employer" ? (
-            <></>
-          ) : (
-            <Link to={`/application/${job._id}`}>Apply Now</Link>
-          )}
+          </div>
         </div>
+
       </div>
     </section>
   );
